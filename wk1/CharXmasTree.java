4c9f4a513a50d8e442cf721d7d9599e0611252f9
jerry <jerry.ye0110@gmail.com>
2020-09-11 15:49:32 +0800

    first commit

---
+++ wk1/CharXmasTree.java
@@ -0,0 +1,15 @@
+
+/**
+* CharXmasTree  
+*/
+public class CharXmasTree {
+
+	public static void main(String[] args) {
+	    System.out.println("   *   ");
+	    System.out.println("  / \\  ");
+	    System.out.println(" /   \\ ");
+	    System.out.println("/     \\");
+	    System.out.println("--| |--");
+	    System.out.println("-------");
+	}
+}
